4af9d68f31c0a4914fab3790ba681c7c2a430b60
Chikara <chtakahashi10@gmail.com>
2021-08-13 11:29:06 -0400

    use core/artifact to upload json. next will be using artifact to download it for the render task def step

--- src/main/getTaskDefinition.js
+++ src/main/getTaskDefinition.js
@@ -1,17 +1,33 @@
 const core = require('@actions/core');
+const artifact = require('@actions/artifact')
 const aws = require('aws-sdk');
 const fs = require('fs');
-const path = require('path');
 
 async function run() {
   const taskDefinition = core.getInput('TASK_DEFINITION', { required: true });
   try {
+    // Connect to ecs and pull the task definition
     const ecs = new aws.ECS({
       customUserAgent: 'amazon-ecs-deploy-task-definition-for-github-actions'
     });
     const taskDefResponse = await ecs.describeTaskDefinition({ taskDefinition }).promise();
-    const filePath = path.join(process.env.GITHUB_WORKSPACE, `task-definition.json`)
-    await fs.writeFile(filePath, JSON.stringify(taskDefResponse.taskDefinition))    
+
+    // Generate json
+    await fs.writeFile('task-definition.json', JSON.stringify(taskDefResponse.taskDefinition))
+    
+    // Upload as Artifact
+    const artifactClient = artifact.create()
+    const artifactName = 'taskDefinition'
+    const files = [
+      'task-definition.json'
+    ]
+
+    const rootDirectory = '.'
+    const options = {
+      continueOnError: false
+    }
+    await artifactClient.uploadArtifact(artifactName, files, rootDirectory, options)
+
   } catch (error) {
     core.setFailed(error.message);
   }
