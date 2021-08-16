c97bf0fc688ff462a5b0a6dc4f7cbc3a5f4d0056
Chikara <chtakahashi10@gmail.com>
2021-08-16 16:21:17 -0400

    add service count increment

--- src/main/index.js
+++ src/main/index.js
@@ -2,13 +2,14 @@ const configAwsCreds = require('./configAwsCreds')
 const amazonEcrLogin = require('./amazonEcrLogin')
 const getTaskDefinition = require('./getTaskDefinition')
 const renderTaskDefinition = require('./renderTaskDefinition')
+const incrementServiceCount = require('./incrementServiceCount')
 
 async function run() {
   await configAwsCreds()
   const imageUri = await amazonEcrLogin()
   const taskDef = await getTaskDefinition()
   const newTaskDef = await renderTaskDefinition(taskDef, imageUri)
-  console.log(newTaskDef)
+  await incrementServiceCount()
 }
 
 module.exports = run;
