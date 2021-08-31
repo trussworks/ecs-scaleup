bfe7502d757186820a344b926a1edd84ad5520f7
Chikara <chtakahashi10@gmail.com>
2021-08-31 14:49:42 -0400

    simplify renderTaskDefinition into its bare bones components of just updating the image URI and passing along the new TD

--- src/main/renderTaskDefinition.test.js
+++ src/main/renderTaskDefinition.test.js
@@ -22,177 +22,29 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
 const run = require('./renderTaskDefinition');
-const core = require('@actions/core');
 
 jest.mock('@actions/core');
-
-const FAKE_TASK_DEFINITION = 'fake-task-definition'
-const FAKE_IMAGE_URI = 'fake-image-uri'
 
 describe('Render task definition', () => {
 
     beforeEach(() => {
         jest.clearAllMocks();
-
-        core.getInput = jest
-            .fn()
-            .mockReturnValueOnce('task-definition.json') // task-definition
-            .mockReturnValueOnce('web')                  // container-name
-            .mockReturnValueOnce('nginx:latest');        // image
-
-        process.env = Object.assign(process.env, { GITHUB_WORKSPACE: __dirname });
-        process.env = Object.assign(process.env, { RUNNER_TEMP: '/home/runner/work/_temp' });
-
-        // tmp.fileSync.mockReturnValue({
-        //     name: 'new-task-def-file-name'
-        // });
-
-        // fs.existsSync.mockReturnValue(true);
-
-        // jest.mock('./task-definition.json', () => ({
-        //     family: 'task-def-family',
-        //     containerDefinitions: [
-        //         {
-        //             name: "web",
-        //             image: "some-other-image"
-        //         },
-        //         {
-        //             name: "sidecar",
-        //             image: "hello"
-        //         }
-        //     ]
-        // }), { virtual: true });
     });
 
-    // test('renders the task definition and creates a new task def file', async () => {
-    //     await run();
-    //     expect(tmp.fileSync).toHaveBeenNthCalledWith(1, {
-    //         tmpdir: '/home/runner/work/_temp',
-    //         prefix: 'task-definition-',
-    //         postfix: '.json',
-    //         keep: true,
-    //         discardDescriptor: true
-    //       });
-    //     expect(fs.writeFileSync).toHaveBeenNthCalledWith(1, 'new-task-def-file-name',
-    //         JSON.stringify({
-    //             family: 'task-def-family',
-    //             containerDefinitions: [
-    //                 {
-    //                     name: "web",
-    //                     image: "nginx:latest"
-    //                 },
-    //                 {
-    //                     name: "sidecar",
-    //                     image: "hello"
-    //                 }
-    //             ]
-    //         }, null, 2)
-    //     );
-    //     expect(core.setOutput).toHaveBeenNthCalledWith(1, 'task-definition', 'new-task-def-file-name');
-    // });
-
-    // test('renders a task definition at an absolute path', async () => {
-    //     core.getInput = jest
-    //         .fn()
-    //         .mockReturnValueOnce('/hello/task-definition.json') // task-definition
-    //         .mockReturnValueOnce('web')                  // container-name
-    //         .mockReturnValueOnce('nginx:latest');        // image
-    //     jest.mock('/hello/task-definition.json', () => ({
-    //         family: 'task-def-family',
-    //         containerDefinitions: [
-    //             {
-    //                 name: "web",
-    //                 image: "some-other-image"
-    //             }
-    //         ]
-    //     }), { virtual: true });
-
-    //     await run();
-
-    //     expect(tmp.fileSync).toHaveBeenNthCalledWith(1, {
-    //         tmpdir: '/home/runner/work/_temp',
-    //         prefix: 'task-definition-',
-    //         postfix: '.json',
-    //         keep: true,
-    //         discardDescriptor: true
-    //       });
-    //     expect(fs.writeFileSync).toHaveBeenNthCalledWith(1, 'new-task-def-file-name',
-    //         JSON.stringify({
-    //             family: 'task-def-family',
-    //             containerDefinitions: [
-    //                 {
-    //                     name: "web",
-    //                     image: "nginx:latest"
-    //                 }
-    //             ]
-    //         }, null, 2)
-    //     );
-    //     expect(core.setOutput).toHaveBeenNthCalledWith(1, 'task-definition', 'new-task-def-file-name');
-    // });
-
-    // test('error returned for missing task definition file', async () => {
-    //     fs.existsSync.mockReturnValue(false);
-    //     core.getInput = jest
-    //         .fn()
-    //         .mockReturnValueOnce('does-not-exist-task-definition.json')
-    //         .mockReturnValueOnce('web')
-    //         .mockReturnValueOnce('nginx:latest');
-
-    //     await run();
-
-    //     expect(core.setFailed).toBeCalledWith('Task definition file does not exist: does-not-exist-task-definition.json');
-    // });
-
-    test('error returned for non-JSON task definition contents', async () => {
-        jest.mock('./non-json-task-definition.json', () => ("hello"), { virtual: true });
-
-        core.getInput = jest
-            .fn()
-            .mockReturnValueOnce('non-json-task-definition.json')
-            .mockReturnValueOnce('web')
-            .mockReturnValueOnce('nginx:latest');
-
-        await run();
-
-        expect(core.setFailed).toBeCalledWith('Invalid task definition format: containerDefinitions section is not present or is not an array');
-    });
-
-    test('error returned for malformed task definition with non-array container definition section', async () => {
-        jest.mock('./malformed-task-definition.json', () => ({
-            family: 'task-def-family',
-            containerDefinitions: {}
-        }), { virtual: true });
-
-        core.getInput = jest
-            .fn()
-            .mockReturnValueOnce('malformed-task-definition.json')
-            .mockReturnValueOnce('web')
-            .mockReturnValueOnce('nginx:latest');
-
-        await run();
-
-        expect(core.setFailed).toBeCalledWith('Invalid task definition format: containerDefinitions section is not present or is not an array');
-    });
-
-    test('error returned for task definition without matching container name', async () => {
-        jest.mock('./missing-container-task-definition.json', () => ({
-            family: 'task-def-family',
+    test('actually changes the contents of the task Definition', () => {
+        const fakeTaskDefinition = {
+            family: 'fake-task-def-family',
             containerDefinitions: [
                 {
-                    name: "main",
-                    image: "some-other-image"
+                    name: 'fake-definition-name',
+                    image: 'a-fake-image'
                 }
             ]
-        }), { virtual: true });
-
-        core.getInput = jest
-            .fn()
-            .mockReturnValueOnce('missing-container-task-definition.json')
-            .mockReturnValueOnce('web')
-            .mockReturnValueOnce('nginx:latest');
-
-        await run();
-
-        expect(core.setFailed).toBeCalledWith('Invalid task definition: Could not find container definition with matching name');
+        }
+        const fakeImageURI = 'a-different-fake-image'
+        const newTaskDefinition = run(fakeTaskDefinition, fakeImageURI)
+        expect(JSON.parse(newTaskDefinition)).toHaveProperty('containerDefinitions')
+        expect(JSON.parse(newTaskDefinition).containerDefinitions).toHaveLength(1)
+        expect(JSON.parse(newTaskDefinition).containerDefinitions[0].image).toBe('a-different-fake-image')
     });
 });
