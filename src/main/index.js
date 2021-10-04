901ec9c611a2ae32af75ca19b939ee00311e95eb
Chikara <chtakahashi10@gmail.com>
2021-10-04 15:13:03 -0400

    test removing ecr login and image specification

--- src/main/index.js
+++ src/main/index.js
@@ -1,17 +1,13 @@
 const configAwsCreds = require('./configAwsCreds')
-const amazonEcrLogin = require('./amazonEcrLogin')
 const getTaskDefinition = require('./getTaskDefinition')
-const renderTaskDefinition = require('./renderTaskDefinition')
 const incrementServiceCount = require('./incrementServiceCount')
 const deployTaskDefinition = require('./deployTaskDefinition')
 
 async function run() {
   await configAwsCreds()
-  const imageUri = await amazonEcrLogin()
   const taskDef = await getTaskDefinition()
-  const newTaskDef = await renderTaskDefinition(taskDef, imageUri)
   await incrementServiceCount()
-  await deployTaskDefinition(newTaskDef)
+  await deployTaskDefinition(taskDef)
 }
 
 module.exports = run;
